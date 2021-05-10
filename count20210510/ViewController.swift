927e3e5b47a08580d413cf227dd91a36e01c1e70
Kony <fzf55747@kwansei.ac.jp>
2021-05-10 19:33:07 +0900

    Initial Commit

---
+++ count20210510/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  count20210510
+//
+//  Created by 小西星七 on 2021/05/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
